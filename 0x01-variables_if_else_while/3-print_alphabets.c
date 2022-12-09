44c3254de14fef38296582074063c579ae9bdffc
HaderAli2018 <HaderAli2018@github.com>
2022-12-09 21:59:29 +0200

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  main -  Entry point
+ *
+ * Return: Always 0 if (Success)
+ */
+int main(void)
+{
+	char c = 'a', d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+	}
+	while (d < = 'Z')
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
