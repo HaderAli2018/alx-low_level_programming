572c9dc4f4d78072afbae6ad8e8066de2f358214
HaderAli18 <haderyoussef18@github.com>
2022-12-10 23:34:00 +0200

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 /**
- *
- * main - Entry point 
- *
- * Return: 0 If Success
- */
-
+* main - Entry point
+*
+* Return: 0 If Success
+*/
 int main(void)
 {
+	int i;
 
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
