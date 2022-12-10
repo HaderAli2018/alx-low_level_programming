7a6712d8646db627a2c78f4c4f37537af4700a95
HaderAli18 <haderyoussef18@github.com>
2022-12-10 15:03:23 +0200

    task 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point 
+ *
+ * Return: 0 If Success
+ */
+
+int main(void)
+{
+
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: 0 Always if Success
+*/
+int main(void)
+{
+	 int i;
+	 char ascii;
+
+for (i = 122; i > = 97; i--)
+{
+ascii = (char)i;
+putchar(ascii);
+}
+putchar('\n');
+return (0);
+}
