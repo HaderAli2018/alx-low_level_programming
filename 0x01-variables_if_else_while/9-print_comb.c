12deb0c5389cbebeb5a134458a67289a001ba4e8
HaderAli18 <haderyoussef18@github.com>
2022-12-10 16:06:56 +0200

    task 8

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: 0 IF succcess
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 9; x++)
+	{
+		putchar(x);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n')
+	return (0);
+}
