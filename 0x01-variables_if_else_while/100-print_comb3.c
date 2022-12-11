25aed2102d70cad3808a50a2b8873776b1ce046d
HaderAli2018 <HaderAli2018@github.com>
2022-12-11 23:50:29 +0200

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int num1, num2;
+num1 = 48;
+
+while (num1 < 58)
+{
+num2 = 48;
+
+while (num2 < 58)
+{
+putchar(num1 + num2);
+putchar(' ');
+putchar(',');
+}
+}
+putchar('\n');
+return (0);
+}
