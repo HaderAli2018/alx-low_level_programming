35068a860562df514f41e81fb9c508ea5d9039d5
HaderAli2018 <HaderAli2018@github.com>
2022-12-12 07:54:03 +0200

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,11 +13,11 @@ for (i = 0; i < 8; i++)
 for (j = i + 1; j < 9; j++)
 {
 for (k = j + 1; k < 10; k++)
-{digit[0] = i + '0';
-digit[1] = j + '0';
-digit[2] = k + '0';
-digit[3] = '\0';
-printf("%s\n", digit);
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+putchar('\n');
+putchar(',');
 }
 }
 }
