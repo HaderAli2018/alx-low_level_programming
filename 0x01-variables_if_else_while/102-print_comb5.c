60f64d36cec9fac3b5e55403f7c0dd6052503cb4
HaderAli2018 <HaderAli2018@github.com>
2022-12-12 08:21:35 +0200

    task 10

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+*main - entry point
+*
+*Return:0 if Success
+*/
+int main(void)
+{
+int i, j, k , l;
+for (i = 0 ; i < 8 ; i++)
+{
+for (j = i; j <8; j++)
+{
+for (k = j; k < 8; k++)
+{
+for (l = k+1; l < 8; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(' ');
+putchar(k + '0');
+putchar(l + '0');
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
