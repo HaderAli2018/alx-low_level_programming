222eaff0ca82d4fa15983d8f3e9ab4aef47a51e1
HaderAli18 <haderyoussef18@github.com>
2022-12-14 01:28:38 +0200

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*_islower - checks for lowercase character
+*_islower -checks for lowercase
+*Return: 1 if success and 0 if failed
 */
 int _islower(int c)
 {
-	if(islower(c)
-	{
+	if (islower(c)
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
-
