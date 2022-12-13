2c0c6dbe2bb2da4c47142d7cbfbf782da4e410ba
HaderAli18 <haderyoussef18@github.com>
2022-12-14 01:02:41 +0200

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_alphabet_x10 -  prints 10 times the alphabet
+*/
+void print_alphabet_x10(void)
+{
+	char letter;
+	int x;
+
+	for (x = 0 ; x <= 9; x++)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+	}
+}
