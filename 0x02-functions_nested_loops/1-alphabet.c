81e7e7e7bd06286e89bb4bc9cb0478e7021c4337
HaderAli18 <haderyoussef18@github.com>
2022-12-14 00:53:44 +0200

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_alphabet  - Print alphabet in lower case
+*Return: 0 if success
+*/
+void print_alphabet(void)
+{
+	for (char x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
