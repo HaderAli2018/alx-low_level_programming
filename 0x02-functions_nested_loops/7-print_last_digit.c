6a485bd25a20bdee64b7d743b3fb26ac9b94b47f
HaderAli18 <haderyoussef18@github.com>
2022-12-14 02:30:30 +0200

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ *@num: the integer paramter
+ * Return: last Digit
+ */
+int print_last_digit(int num)
+{
+	int digit;
+
+	digit = num % 10;
+	return (digit);
+}
