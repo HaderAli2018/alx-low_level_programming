3e6471033ef1c624bf3c147b1d6cdd97c05ad1e2
HaderAli2018 <HaderAli2018@github.com>
2022-12-15 01:57:28 +0200

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,21 +11,22 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			res = num * j;
+			if (j != 9)
+			{
 			if (res <= 9)
 			{
 			_putchar('0' + res);
+			_putchar(',');
 			_putchar(' ');
 			}
 			else
 			{
 			_putchar('0' + res / 10);
 			_putchar('0' + res % 10);
+			_putchar(',');
 			}
-		if (j != 9)
-		{
-		_putchar(',');
-		_putchar(' ');
-		}
+			_putchar(' ');
+			}
 		}
 		_putchar('\n');
 		num++;
