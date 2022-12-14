98906ebca4122522ffe95595bd5d6d72bd68993e
HaderAli2018 <HaderAli2018@github.com>
2022-12-15 01:16:00 +0200

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,19 @@
 #include "main.h"
+/**
+ * times_table - prints 9 times of time table
+ */
 void times_table(void)
 {
+	int i, j, num = 1;
 
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar(num * j);
+			_putchar('\t');
+		}
+		_putchar('\n');
+		num++;
+	}
+}
