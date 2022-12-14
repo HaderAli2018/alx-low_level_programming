5a2b497d75c913522d8d096b6dc3a53eab465125
HaderAli18 <haderyoussef18@github.com>
2022-12-14 02:22:32 +0200

    task 6wq

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - computes the absolute value of an integer
+ *@num: integer number
+ * Return: return 1 if success
+ */
+int _abs(int num)
+{int abNum;
+abNum = abs(num);
+return (1);
+}
