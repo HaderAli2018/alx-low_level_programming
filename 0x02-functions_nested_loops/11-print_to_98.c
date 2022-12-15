30ea87b226e36dead058ba12652b79b1c6b48740
HaderAli2018 <HaderAli2018@github.com>
2022-12-15 05:21:24 +0200

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,13 +9,23 @@ void print_to_98(int n)
 
 	for (i = n; i <= 98; i++)
 	{
-		if (i <= 10 || i <= 98)
+		if (i >= 10 || i <= 98)
 		{
+			if (i < 0)
+			{
+			n = n + 1;
+			}
 		_putchar('0' + i / 10);
 		_putchar('0' + i % 10);
 		}
 		else
+		{
+			if (i < 0)
+			{
+			i = i + 1;
+			}
 		_putchar('0' + i);
+		}
 	if (i != 98)
 	{
 	_putchar(',');
