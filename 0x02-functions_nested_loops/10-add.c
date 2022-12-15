4db2c3f36ce4416ee927139ee7b537f8998b9752
HaderAli2018 <HaderAli2018@github.com>
2022-12-15 03:55:54 +0200

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add -  function that adds two integers
+ *@x: integer paramter1
+ *@y: integer paramter2
+ *Return: value of add two integer
+ */
+int add(int x, int y)
+{
+	int res;
+
+	res = x + y;
+	_putchar(res + '0');
+	return (res);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,8 +28,8 @@ void times_table(void)
 			if (j != 9)
 			{
 			_putchar(',');
-			}
 			_putchar(' ');
+			}
 			}
 		}
 		_putchar('\n');
