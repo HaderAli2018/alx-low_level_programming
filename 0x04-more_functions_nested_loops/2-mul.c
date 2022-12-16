c2776f8f41c0ad70803b14b41b44a43f11495959
HaderAli2018 <HaderAli2018@github.com>
2022-12-15 17:13:17 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first paramter
+ * @b: second paramter
+ * Return: value of multiple
+ */
+int mul(int a, int b)
+{
+	int multy;
+
+	multy = a * b;
+	return (multy);
+}
