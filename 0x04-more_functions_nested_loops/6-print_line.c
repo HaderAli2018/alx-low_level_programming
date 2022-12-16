9b2f9b0fcf20520cce20bfa64f4809b0672e1133
HaderAli2018 <HaderAli2018@github.com>
2022-12-15 19:53:47 -0800

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,11 +7,12 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (n <= 0)
 			_putchar('\n');
-		_putchar('_');
+		else
+			_putchar('_');
 	}
-_putchar('\n');
+	_putchar('\n');
 }
