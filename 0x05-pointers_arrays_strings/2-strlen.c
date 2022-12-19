3d0a8209c71fd06852ccd4a1bb6a857690f08e99
HaderAli2018 <HaderAli2018@github.com>
2022-12-18 21:40:54 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: character paramter
+ * Return: int value
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length =  strlen(s);
+	return (length);
+}
