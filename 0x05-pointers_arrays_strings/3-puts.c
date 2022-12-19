14d40fdf5470e26a774d8f6cc4f25782762b3275
HaderAli2018 <HaderAli2018@github.com>
2022-12-18 23:39:08 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _puts - prints a string, followed by a new line,
+ * @str: character paramter
+ */
+void _puts(char *str)
+{
+	str = "hello pointer program";
+	puts(str);
+}
