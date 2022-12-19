8689d4ce543c497777a7d2877814464c5ad4bd34
HaderAli2018 <HaderAli2018@github.com>
2022-12-19 09:54:36 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints a string, in reverse
+ * @s: character paramter
+ */
+void print_rev(char *s)
+{
+	int i, a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	for (i = a - 1; i <= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
