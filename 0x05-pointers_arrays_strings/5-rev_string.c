20ae45f66aafc81d0a15a71ba8c3940f20a49968
HaderAli2018 <HaderAli2018@github.com>
2022-12-19 10:59:22 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include<string.h>
+/**
+ * rev_string - reverses a string.
+ * @s: character paramter
+ */
+void rev_string(char *s)
+{
+	int i, temp, len, middle;
+
+	len = strlen(s);
+	middle = len / 2;
+	for (i = 0; i <= middle; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
