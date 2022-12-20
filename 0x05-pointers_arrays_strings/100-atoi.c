f6be4388be1c2cb5b0e635eb4fe99f574859ce6f
HaderAli2018 <HaderAli2018@github.com>
2022-12-20 05:31:21 -0800

    task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_atoi - convert a string to an integer.
+ *@s: character paramter
+ *Return: integer value
+ */
+int _atoi(char *s)
+{
+	int x;
+
+	x = atoi(s);
+	return (x);
+}
