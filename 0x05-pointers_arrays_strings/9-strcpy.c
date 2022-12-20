372a3e0b96b7dd55df117c05ee5822f54a27efeb
HaderAli2018 <HaderAli2018@github.com>
2022-12-20 04:42:58 -0800

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy -  copies the string
+ * @dest: character paramter
+ * @src: character paramter
+ * Return: char value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
