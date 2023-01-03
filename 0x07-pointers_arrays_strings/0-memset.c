8da7cdb16d03bc419f50254aa43ab2c9c40cdde1
HaderAli2018 <HaderAli2018@github.com>
2023-01-03 01:35:50 -0800

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _memset - fills memory with a constant byte
+ * @s:char paramter
+ * @b:char paramter
+ * @n: int paramter
+ * Return:pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+	return (s);
+}
