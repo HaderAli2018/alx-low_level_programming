bf2630edd21a7b76723c7867e5ff1fbafba28466
HaderAli2018 <HaderAli2018@github.com>
2023-01-04 22:28:59 -0800

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,18 @@
 #include"main.h"
+#include<string.h>
 /**
  *  _puts_recursion - prints a string
  *  @s: char paramter
  */
 void _puts_recursion(char *s)
 {
-_putchar(*s);
+char *p;
+char c;
+
+p = s;
+if (*p != '\0')
+c = *p;
+_putchar(c);
+p++;
+_puts_recursion(p);
 }
