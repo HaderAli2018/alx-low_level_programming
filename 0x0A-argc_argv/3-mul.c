0d5636286036a21f9aadba798b5343aa895ccd3b
HaderAli2018 <HaderAli2018@github.com>
2023-01-09 15:41:37 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-int mult = 1;
+int mult, num1, num2;
 
 if (argc < 2)
 {
@@ -20,7 +20,9 @@ return (1);
 }
 else
 {
-mult = atoi(argv[1]) * atoi(argv[2]);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+mult = (num1*num2);
 printf("%d", mult);
 printf("\n");
 return (0);
