475bc4e62a0c2a95e497b20f04c0f17d99a0b8f9
HaderAli2018 <HaderAli2018@github.com>
2023-01-09 13:36:11 -0800

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,12 @@
 #include<stdio.h>
+#include<string.h>
 /**
  * main - main function
  *@argc: int paramter
  */
 int main(int argc, char *argv[])
 {
-int i ;
-
-for (i = 0; i < argc; i++)
-{
-printf("argv[%2d]: %s\n", i, argv[i]);
-}
+(void) argv;
+printf("%d\n", argc);
 return (0);
 }
