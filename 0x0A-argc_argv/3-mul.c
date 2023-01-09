409079757f5b5c640f82000042f64917fbbbfcbd
HaderAli2018 <HaderAli2018@github.com>
2023-01-09 15:19:38 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-int mult = 1, i;
+int mult = 1;
 
 if (argc < 2)
 {
@@ -19,10 +19,7 @@ return (1);
 }
 else
 {
-for (i = 1; i < argc; i++)
-{
-mult *= atoi(argv[i]);
-}
+mult = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", mult);
 return (0);
 }
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,2 +1,3 @@
 0. It ain't what they call you, it's what you answer to
 1. Silence is argument carried out by other means
+3. Neither irony nor sarcasm is argument
