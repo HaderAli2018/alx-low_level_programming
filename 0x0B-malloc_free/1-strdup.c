c4a0e844607c38ccdd3c7ea18432e5f99f5337c9
HaderAli2018 <HaderAli2018@github.com>
2023-01-10 01:05:13 -0800

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,6 @@
 #include"main.h"
+#include<string.h>
+#include<stdlib.h>
 /**
  * _strdup -  returns a pointer to a newly allocated space
  * @str: char paramter
@@ -6,5 +8,21 @@
  */
 char *_strdup(char *str)
 {
-
+char *ptr = (char *)malloc(1 * (sizeof(str)));
+if (ptr == 0)
+{
+return (NULL);
+}
+else
+{
+if (str == NULL)
+{
+return (NULL);
+}
+else
+{
+ptr = str;
+return (ptr);
+}
+}
 }
