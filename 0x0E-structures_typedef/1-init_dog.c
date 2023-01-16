197c94f1a4cbe43a2e0ffa439e53537645393a19
HaderAli2018 <HaderAli2018@github.com>
2023-01-16 08:01:28 +0200

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include"dog.h"
+/**
+ * init_dog - initialize a variable of type struct dog
+ *@name: char paramter
+ *@age: float paramter
+ *@owner: char aparmter
+ *@d: struct paramter
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog old_one;
+d = &old_one;
+(*d).name = old_one.name;
+(*d).age = old_one.age;
+(*d).owner = old_one.owner;
+}
