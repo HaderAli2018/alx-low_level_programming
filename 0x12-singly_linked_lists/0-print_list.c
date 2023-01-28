05a9f1c53eb79e655face211135e2c8a46670666
HaderAli2018 <HaderAli2018@github.com>
2023-01-27 22:22:45 -0800

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,10 +5,26 @@
  *@h: struct paramter
  *@Return: all element of list
  */
-typedef struct list_t
+typedef struct node
 {
-}h;
+char *str;
+int len;
+struct node *next;
+} list_t;
 size_t print_list(const list_t *h)
 {
-
+size_t count;
+list_t *current_node = h;
+while (current_node != NULL)
+{
+if (current_node->str == NULL)
+{
+current_node->len = 0;
+current_node->str = "(nill)";
+}
+printf("[%d] %s", current_node->len, current_node->str);
+count++;
+current_node = current_node->next;
+}
+return (count);
 }
