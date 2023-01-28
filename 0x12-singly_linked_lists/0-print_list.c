92aa4eb2d87c562a03c1ccd1a8988d31d63c0b54
HaderAli2018 <HaderAli2018@github.com>
2023-01-27 20:48:07 -0800

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include"lists.h"
+/**
+ *print_list -  prints all the elements of a list_t list
+ *@h: struct paramter
+ *@Return: all element of list
+ */
+typedef struct list_t
+{
+}h;
+size_t print_list(const list_t *h)
+{
+
+}
